542f58312afa4b0a0ff732fcac38ac011a83dfcb
Himanshu Rana <himanshu.rana@compunneldigital.com>
2024-10-15 15:46:57 +0530

    Add more pages

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 
 pdf = FPDF(orientation="P",unit = "mm",format="A4")
+pdf.set_auto_page_break(auto=False,margin=0 )
 
 df = pd.read_csv("topics (1).csv")
 
@@ -10,11 +11,23 @@ for index,row in df.iterrows():
     pdf.add_page()
 
     pdf.set_font(family="Times",style="B",size=24 )
-    pdf.set_text_color(254,0,0)
+    pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"],align="L",ln=1)
     pdf.line(10,21,200,21)
+
+    pdf.ln(265)
+    #  set the footer
+    pdf.set_font(family="Times",style="I",size=8)
+    pdf.set_text_color(180,180,180)
+    pdf.cell(w=0, h=10, text=row["Topic"], align="R")
+
     
     for i in range(row["Pages"]-1):
         pdf.add_page()
+
+         #  set the footer
+        pdf.set_font(family="Times",style="I",size=8)
+        pdf.set_text_color(180,180,180)
+        pdf.cell(w=0,h=10,text= row["Topic"], align="R")
 
 pdf.output("output.pdf")
